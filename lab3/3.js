dfa513bc879a1cbd819c53e109bc49d850f34e93
rashmivtl289 <98947167+rashmivtl289@users.noreply.github.com>
2022-07-19 19:05:29 +0530

    Add files via upload

---
+++ lab3/3.js
@@ -0,0 +1,32 @@
+function fibo() {
+    var n = parseInt(prompt("Enter the number for fibonacci: "));
+    var f1 = 0, f2 = 1, f3;
+    document.write("<h2>Fibonacci Sequence</h2><br>");
+    if (n == 0) {
+        document.write("Enter number greater than zero");
+    }
+    else if (n == 1) {
+        document.write(f1);
+    }
+    else if (n == 2) {
+        document.write(f1 + " " + f2);
+    }
+    else {
+        document.write(f1 + " " + f2)
+        for (var i = 0; i < n - 2; i++) {
+            f3 = f1 + f2;
+            document.write(" " + f3);
+            f1 = f2;
+            f2 = f3;
+        }
+    }
+}
+
+function firstN() {
+var n = parseInt(prompt("Enter the limit: "));
+var string = "";
+for (var i = 1; i <= n; i++) {
+    string += i.toString() + " | " + (i * i).toString() + "\n";
+}
+alert(string);
+}
